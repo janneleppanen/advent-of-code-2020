12b44ad6040654f7d95c3b5b31710f2cbd70fa8b
Janne Leppänen <janne@janneleppanen.com>
2020-12-06 10:17:32 +0200

    Add day 6 solution

---
+++ day-6/index.js
@@ -0,0 +1,45 @@
+const fs = require('fs');
+fs.readFile(`${__dirname}/input.txt`, 'utf-8', (error, input) => run(input))
+
+
+
+const run = (input) => {
+  const groups = input.split('\n\n');
+  
+  const firstAnswer = groups
+    .map(group => {
+      return group.replace(/\n/g, '');
+    })
+    .map(group => {
+      return [...new Set(group.split(''))].join('')
+    })
+    .reduce((sum, i) => {
+      return sum + i.length
+    }, 0)
+  console.log(`Answer to first is ${firstAnswer}`)
+
+  const secondAnswer = groups.map((group) => {
+    const groupSize = group.split('\n').length;
+    const answerCountMap = group
+        .replace(/\n/g, '')
+        .split('')
+        .sort()
+        .reduce((map, char) => {
+          if (!map.hasOwnProperty(char)) {
+            map[char] = 0;
+          }
+          map[char]++;
+          
+          if (map[char] === groupSize) {
+            map.yesToAll.push(char)
+          }
+
+          return map;
+        }, { yesToAll: [] });
+      
+    return answerCountMap.yesToAll.length;
+  })
+  .reduce((sum, i) => sum + i, 0);
+
+  console.log(`Answer to first is ${secondAnswer}`)
+}
